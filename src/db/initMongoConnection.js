a2ea78623412aeff4284b68884bb7fc1a4f78f8e
Yuliya Bondarenko <juliyaodessa84@gmail.com>
2025-03-18 14:43:07 +0100

    fix mongo_user

--- src/db/initMongoConnection.js
+++ src/db/initMongoConnection.js
@@ -3,10 +3,10 @@ import { getEnvVar } from '../utils/getEnvVar.js';
 
 export const initMongoConnection = async () => {
   try {
-    const name = getEnvVar('MONGODB_NAME');
+    const name = getEnvVar('MONGODB_USER');
     const pwd = getEnvVar('MONGODB_PASSWORD');
     const url = getEnvVar('MONGODB_URL');
-    const db = getEnvVar('MONGODB_DB');
+    const db = getEnvVar('MONGO_DB');
     await mongoose.connect(
       `mongodb+srv://${name}:${pwd}@${url}/${db}?retryWrites=true&w=majority`,
     );
