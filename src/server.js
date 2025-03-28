4371b652c8338400e5b7894814156d0c1c64e690
Yuliya Bondarenko <juliyaodessa84@gmail.com>
2025-03-28 14:09:13 +0100

    work done

--- src/server.js
+++ src/server.js
@@ -1,14 +1,25 @@
+import express from 'express';
 import pino from 'pino-http';
 import cors from 'cors';
+
 import { getEnvVar } from './utils/getEnvVar.js';
-import express from 'express';
-import { getContacts, getContactById } from './services/contacts.js';
+import contactsRouter from './routers/contacts.js';
+import { errorHandler } from './middlewares/errorHandler.js';
+import { notFoundHandler } from './middlewares/notFoundHandler.js';
 
 const PORT = process.env.PORT || getEnvVar('PORT');
 
 export const setupServer = () => {
   const app = express();
-  app.use(express.json());
+
+  app.use(
+    express.json({
+      type: ['application/json', 'application/vnd.api+json'],
+      limit: '100kb',
+    }),
+  );
+
+  app.use(cors());
 
   app.use(
     pino({
@@ -18,44 +29,17 @@ export const setupServer = () => {
     }),
   );
 
-  app.use(cors());
-
-  app.get('/contacts', async (req, res) => {
-    const contacts = await getContacts();
-    res.status(200).json({
-      message: 'Successfully found contacts!',
-      data: { contacts },
+  app.get('/', (req, res) => {
+    res.json({
+      message: 'Contacts ready!',
     });
   });
 
-  app.get('/contacts/:contactId', async (req, res, next) => {
-    const { contactId } = req.params;
-    const contact = await getContactById(contactId);
-    if (!contact) {
-      res.status(404).json({
-        message: 'Contact not found',
-      });
-      return;
-    }
-    res.status(200).json({
-      message: 'Successfully found contact with id{contactId}!',
-      data: { contact },
-    });
-    return;
-  });
+  app.use(contactsRouter);
 
-  app.use('*', (req, res, next) => {
-    res.status(404).json({
-      message: 'Not found',
-    });
-  });
+  app.use('*', notFoundHandler);
 
-  app.use((err, req, res, next) => {
-    res.status(500).json({
-      message: 'Something went wrong!',
-      error: err.message,
-    });
-  });
+  app.use(errorHandler);
 
   app.listen(PORT, () => {
     console.log(`Server is running on port ${PORT}`);
--- src/services/contacts.js
+++ src/services/contacts.js
@@ -1,11 +1,30 @@
-import { contact } from '../models/contact.js';
+import { Contact } from '../models/contact.js';
 
 export const getContacts = async () => {
-  const contacts = await contact.find();
+  const contacts = await Contact.find();
   return contacts;
 };
 
 export const getContactById = async (contactId) => {
-  const contact = await contact.findById(contactId);
+  const contact = await Contact.findById(contactId);
   return contact;
+};
+
+export const createContact = async (contactData) => {
+  const createdContact = await Contact.create(contactData);
+  return createdContact;
+};
+
+export const deleteContact = async (contactId) => {
+  const deletedContact = await Contact.findOneAndDelete({ _id: contactId });
+  return deletedContact;
+};
+
+export const patchContact = async (contactId, contactData) => {
+  const updateContact = await Contact.findOneAndUpdate(
+    { _id: contactId },
+    contactData,
+    { new: true },
+  );
+  return updateContact;
 };
