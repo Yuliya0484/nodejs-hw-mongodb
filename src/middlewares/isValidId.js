399f8925bf44e7238c266f88a8212f199a7e5674
Yuliya Bondarenko <juliyaodessa84@gmail.com>
2025-04-02 12:33:17 +0200

    change validateBody

--- src/middlewares/isValidId.js
+++ src/middlewares/isValidId.js
@@ -4,7 +4,7 @@ import createHttpError from 'http-errors';
 export function isValidId(req, _res, next) {
   const { id } = req.params;
 
-  if (!id || isValidObjectId(id)) {
+  if (id || isValidObjectId(id)) {
     return next(createHttpError.BadRequest('ID is not valid'));
   }
 
